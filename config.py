f285be7df4e061d3de2616c121e7f95527dbf5dd
MuradHasanov07 <Muradhasanov1708@gmail.com>
2025-05-15 13:27:10 +0300

    duzenleme

--- config.py
+++ config.py
@@ -2,16 +2,20 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'gizli-anahtar-buraya'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:M-17082005-H@127.0.0.1:3306/p2p_transfer'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'mysql+pymysql://root:M-17082005-H@127.0.0.1:3306/p2p_transfer'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER') or \
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 2 * 1024 * 1024 * 1024  # 2GB
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    FLASK_ENV = 'development'
 
 class ProductionConfig(Config):
     DEBUG = False
+    FLASK_ENV = 'production'
 
 config = {
     'development': DevelopmentConfig,
